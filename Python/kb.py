dd88805919c84eba8763c013ad0ff5c9b6f0bafe
TibitFromTaiwan <jim870718@gmail.com>
2020-03-17 18:58:17 +0800

    .

--- Python/kb.py
+++ Python/kb.py
@@ -1,30 +1,35 @@
 import RPi.GPIO as GPIO
 from time import sleep
 
-GPIO.setmode(GPIO.BOARD)
+key = ('1', '2', '3', '4', '5', '6', '7', '8', '9', '*', '0', '#')
 
 scan_pins = (24, 22, 18, 16)
 read_pins = (12, 10, 8)
 
-for pin in scan_pins: GPIO.setup(pin, GPIO.OUT)
-for pin in read_pins: GPIO.setup(pin, GPIO.IN)
+def kb_init():
+	GPIO.setmode(GPIO.BOARD)
+	for pin in scan_pins: GPIO.setup(pin, GPIO.OUT)
+	for pin in read_pins: GPIO.setup(pin, GPIO.IN)
 
-try:
-	while True:
-		key_status = []
-		for i in range(4):
-			GPIO.output(scan_pins[i], True)
+def kb_scan():
+	key_status = []
+	for i in range(4):
+		GPIO.output(scan_pins[i], True)
 
-			tmp = []
-			for j in range(3):
-				tmp.append(1 if GPIO.input(read_pins[j]) else 0)
+		for j in range(3):
+			key_status.append(1 if GPIO.input(read_pins[j]) else 0)
 
-			GPIO.output(scan_pins[i], False)
+		GPIO.output(scan_pins[i], False)
 
-			key_status.append(tmp)
-			sleep(0.01)
+		sleep(0.01)
 
 		print(key_status)
+		if key_status.count(1) == 1:
+			print(key[key_status.index(1)])
+			
+try:
+	while True:
+		kb_scan()
 
 finally:
 	GPIO.cleanup()
