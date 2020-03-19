46849a5ca01957aaf68daa1d6840795196e993ee
TibitFromTaiwan <jim870718@gmail.com>
2020-03-19 19:05:28 +0800

    .

--- Python/main.py
+++ Python/main.py
@@ -10,10 +10,10 @@ GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
 YELLOW = (255 ,255, 0)
 ##### control value #####
-gun_speed = 0
-solder_speed = 0
-gun_height = 0
-v, a = 0, 0
+gun_speed_value = 0
+solder_speed_value = 0
+gun_height_value = 0
+v_value, a_value = 0, 0
 #########################
 
 ####### iniaialize interface(pygame) #########
@@ -21,20 +21,30 @@ pg.init()
 pg.font.init()
 
 font = pg.font.SysFont('Noto Sans CJK', 40)
+font_small = pg.font.SysFont('Noto Sans CJK', 24)
+
 W, H = 370, 272
 WIN = pg.display.set_mode((W, H))
 # WIN = pg.display.set_mode((W, H), pg.FULLSCREEN)
+########### initialize connect ###############
+plc_main = plc()
 
 ####### functions #######
-def print_text(text:str, position:int, color = YELLOW):
+def print_text(text:str, position:int, line:bool = 0, color = YELLOW):
     if type(text) != str: 
         print('Text type must be string.')
         return
 
-    text = font.render(text, True, color)
+    if not line:
+        text = font.render(text, True, color)
+        y = 210
+    else:
+        text = font_small.render(text, True, color)
+        y = 240
+
     width = text.get_width()
     x = W//8 * (position*2 - 1) - width//2
-    WIN.blit(text, (x, 220))
+    WIN.blit(text, (x, y))
 
 #########　main loop #########
 run = True
@@ -45,24 +55,29 @@ while run:
 
         # value_key event listener
         if event.type == pg.KEYDOWN:
-            if event.key == pg.K_q: gun_speed += 1
-            if event.key == pg.K_w: solder_speed += 1
-            if event.key == pg.K_e: gun_height += 1
-            if event.key == pg.K_r: v += 1
-            if event.key == pg.K_t: a += 1
-            if event.key == pg.K_a: gun_speed -= 1
-            if event.key == pg.K_s: solder_speed -= 1
-            if event.key == pg.K_d: gun_height -= 1
-            if event.key == pg.K_f: v -= 1
-            if event.key == pg.K_g: a -= 1
+            if event.key == pg.K_q: gun_speed_value += 1
+            if event.key == pg.K_w: solder_speed_value += 1
+            if event.key == pg.K_e: gun_height_value += 1
+            if event.key == pg.K_r: v_value += 1
+            if event.key == pg.K_t: a_value += 1
+            if event.key == pg.K_a: gun_speed_value -= 1
+            if event.key == pg.K_s: solder_speed_value -= 1
+            if event.key == pg.K_d: gun_height_value -= 1
+            if event.key == pg.K_f: v_value -= 1
+            if event.key == pg.K_g: a_value -= 1
 
     WIN.fill(BLACK)
 
-    print_text(str(gun_speed), 1)
-    print_text(str(solder_speed), 2)
-    print_text(str(gun_height), 3)
-    print_text(f'{str(v)}/{str(a)}', 4)
+    print_text(str(gun_speed_value), 1)
+    print_text('mm/min', 1, 1)
+    print_text(str(solder_speed_value), 2)
+    print_text('mm/min', 2, 1)
+    print_text(str(gun_height_value), 3)
+    print_text('mm', 3, 1)
+    print_text(f'{str(v_value)}/{str(a_value)}', 4)
+    print_text('V/A', 4, 1)
 
     pg.display.update()
+    pg.time.delay(100)
 
 pg.quit()
--- Python/modbus.py
+++ Python/modbus.py
@@ -1,9 +1,9 @@
 from pymodbus.client.sync import ModbusTcpClient
 
-GUN_SPEED = 0
-SOLDER_SPEED = 1
+GUN_SPEED_VALUE = 0
+SOLDER_SPEED_VALUE = 1
 
-class client:
+class plc:
     def __init__(self, ip:str = '192.168.1.87'):
         self._ip = ip
 
@@ -16,30 +16,29 @@ class client:
         
     def connect(self):
         self.client = ModbusTcpClient(self._ip)
-        try:
-            self.client.read_coil(0)
-            print('Connect Success.')
-        except:
+        if not client.connect():
             print('Connection failed. Please check error and try again.')
+        else:
+            print('Connect Success.')
 
     def disconnect(self):
-        self.client.close()
-        print(f'Connection \'{self._ip}\' closed.')
+        if client.connect():
+            self.client.close()
+            print(f'Connection \'{self._ip}\' closed.')
 
     ###
-    def write_coil_value(self, name, value:int):
-        if name == 0:
-            # 設定走速
-            pass
-        elif name == 1:
-            # 設定焊料速度
-            pass
-        else:
-            pass
-
-    ###
-    def read_coil_value(self, name):
-
-        value = 0
+    def read_value(self, id:int):
+        if id == GUN_SPEED_VALUE: # read coil
+            value = self.client.read_holding_registers(10)
+        elif id == SOLDER_SPEED_VALUE: # read register
+            value = self.client.read_holding_registers(12)
+        else: pass
 
         return value
+
+    def write_value(self, id:int, value:int):
+        if id == GUN_SPEED_VALUE:
+            self.client.write_register(10, value)
+        elif id == SOLDER_SPEED_VALUE:
+            self.client.write_register(10, value)
+        else: pass
